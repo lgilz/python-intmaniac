f879c533bf8aa3b4e87d0aff9ad5eb0d25c10a8f
Axel Bock <mr.axel.bock@gmail.com>
2016-04-20 18:26:04 +0200

    updated tests

--- tests/testv3file.py
+++ tests/testv3file.py
@@ -9,13 +9,15 @@ import unittest
 @unittest.skipUnless(mock_available, "No mocking available")
 class TestV3File(unittest.TestCase):
 
+    patch_pdct = 'intmaniac.maniac_file._prepare_docker_compose_template'
+
     def setUp(self):
         with patch('intmaniac.isfile') as isf:
             isf.return_value = True
             self.config = intmaniac._parse_args(["-e", "cmd=too"])
 
     def test_v3fileformat_full(self):
-        with patch('intmaniac.maniac_file._prepare_docker_compose_template') as pdc:
+        with patch(self.patch_pdct) as pdc:
             pdc.side_effect = lambda x, *args: x
             rv = maniac_file._parsev3(v3_config_0, self.config)
             self.assertEqual(9, len(rv))
@@ -24,7 +26,16 @@ class TestV3File(unittest.TestCase):
                                 .format(tr.name))
 
     def test_v3fileformat_minimal(self):
-        with patch('intmaniac.maniac_file._prepare_docker_compose_template') as pdc:
+        with patch(self.patch_pdct) as pdc:
             pdc.side_effect = lambda x, *args: x
             rv = maniac_file._parsev3(v3_config_minimal, self.config)
             self.assertEqual(1, len(rv))
+
+    def test_v3_cmdline_propagation(self):
+        with patch(self.patch_pdct) as pdc:
+            pdc.side_effect = lambda x, *args: x
+            rv = maniac_file._parsev3(v3_config_minimal, self.config)
+            self.assertEqual(1, len(rv))
+        self.assertTrue(pdc.call_count > 0)
+        called = pdc.mock_calls[0][1][1]
+        self.assertDictEqual({'cmd': 'too'}, called)
