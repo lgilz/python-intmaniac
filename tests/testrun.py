d61affa11dc61ac7d87d1aa2c947258956f4f645
Axel Bock <mr.axel.bock@gmail.com>
2016-06-14 17:01:40 +0200

    added tests for exception handling

--- tests/testrun.py
+++ tests/testrun.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import intmaniac
+from intmaniac.tools import RunCommandError
 from tests.mocksetup import *
 from tests.configsetup import get_test_tr
 
@@ -56,6 +58,37 @@ class TestTestrun(unittest.TestCase):
                                   environment={'TARGET_URL': 'rsas'}),
                              mock_cc.call_args)
 
+    @unittest.skipUnless(mock_available, "No mocking available")
+    def test_exception_handling(self):
+        # set up base objects
+        argconfig0 = intmaniac._parse_args("-e my_image=my/image".split())
+        argconfig1 = intmaniac._parse_args("-e my_image=my/image "
+                                           "--no-format-output".split())
+        tr_obj = get_test_tr('nocommands')
+        # configure test env
+        tmp0 = MagicMock()
+        tmp1 = ("sim_command", 0, "out", "err")
+        tr_obj.compose_wrapper = tmp0
+        tmp0.up.return_value = [("asdf_two_1", "two"), ]
+        tmp0.kill.return_value = tmp1
+        tmp0.stop.return_value = tmp1
+        tmp0.rm.return_value = tmp1
+        for argconfig in (argconfig0, argconfig1):
+            for side_effect in (
+                RunCommandError(returncode=1, stdout="RCError test",
+                                command=["totally false"]),
+                KeyError("KeyError test"), OSError("OSerror test"),
+            ):
+                with patch('configsetup.tr.run_command') as mock_rc, \
+                        patch('configsetup.tr.create_container') as mock_cc, \
+                        patch('configsetup.tr.get_client') as mock_gc:
+                    mock_cc.return_value = '0815'
+                    mock_gc.return_value.inspect_container.return_value = {
+                        'State': {'ExitCode': 0}
+                    }
+                    mock_rc.side_effect = side_effect
+                    intmaniac._run_tests(argconfig, [tr_obj])
+                    self.assertFalse(tr_obj.succeeded())
 
 
 if __name__ == "__main__":
